03159a76a336ace3fa73e91fda7cb2d80829d79b
Deepak Goyal <dg887180@gmail.com>
2022-04-16 00:26:50 +0530

    feat added onboarding page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { OnboardingPage } from './Pages/OnboardingPage/OnbordingPage';
+
+function App() {
+  return (
+    <div>
+      <OnboardingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Utils/getMinuteBelowTen.js
@@ -0,0 +1,8 @@
+export const getMinuteBelowTen = () => {
+  const minutes = new Date().getMinutes();
+  if (minutes > 0 && minutes <= 9) {
+    return "0" + minutes.toString();
+  } else {
+    return minutes;
+  }
+};
