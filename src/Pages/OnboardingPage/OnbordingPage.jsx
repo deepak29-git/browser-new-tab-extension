b86ea650245c90eb4d3252477642a61bd1925631
Deepak Goyal <dg887180@gmail.com>
2022-04-17 14:02:17 +0530

    refactor

--- src/Pages/OnboardingPage/OnbordingPage.jsx
+++ src/Pages/OnboardingPage/OnbordingPage.jsx
@@ -4,76 +4,71 @@ import nextIcon from "../../assets/next.png";
 import { getMinuteBelowTen, greetings } from "../index";
 
 export const OnboardingPage = () => {
-  const [nameInput, setNameInput] = useState("");
+  const [userName, setUserName] = useState("");
+  const [continueBtn, setContinueBtn] = useState(true);
   const [mainFocusInput, setMainFocusInput] = useState("");
-  const [mainFocus, setMainFocus] = useState("");
-  const [show, setShow] = useState(false);
-  const [hideMainFocusOutput, setHideMainFocusOutput] = useState(false);
-  const [hideInput, setHideInput] = useState(true);
-  const [hideMainFocusTitle, setHideMainFocusTitle] = useState(false);
+  const [printMainFocus, setPrintMainFocus] = useState("");
+  const [hideMainFocusInput, setHideMainFocusInput] = useState(true);
 
-  const continueHandler = () => {
-    setShow(true);
-    setHideMainFocusTitle(true);
-    setMainFocusInput("");
+  const continueHandler = (e) => {
+    if (e.key === "Enter") {
+      setContinueBtn(false);
+    }
+    if (e.target.innerText === "Continue") {
+      setContinueBtn(false);
+    }
   };
 
   const addMainFocusHandler = (e) => {
     if (e.key === "Enter") {
-      setMainFocus(mainFocusInput);
-      setHideMainFocusTitle(false);
-      setHideMainFocusOutput(true);
-      setHideInput(false);
+      setPrintMainFocus(mainFocusInput);
+      setHideMainFocusInput(false);
     }
   };
 
   return (
-    <div className="center-align">
-      {!show ? (
-        <h1 className="title">Hello,what's your name?</h1>
-      ) : (
-        <h1 className="title">{`${new Date().getHours()}:${getMinuteBelowTen()}`}</h1>
-      )}
-      {show && (
-        <p className="h1 greeting-text">
-          {greetings()}, {nameInput}.
-        </p>
-      )}
-      {hideMainFocusTitle && (
-        <p className="h3 main-focus-title mt-1">
-          What is your main focus for today?
-        </p>
-      )}
-      {hideMainFocusOutput && (
-        <p className="h3 main-focus-title mt-1">{mainFocus}</p>
-      )}
-      {!show ? (
-        <input
-          onChange={(e) => setNameInput(e.target.value)}
-          className="name-input"
-          type="text"
-        />
-      ) : (
-        hideInput && (
+    <>
+      {continueBtn ? (
+        <div className="center-align">
+          <h1 className="title">Hello,what's your name?</h1>
           <input
-            onChange={(e) => setMainFocusInput(e.target.value)}
+            onChange={(e) => setUserName(e.target.value)}
             className="name-input"
-            onKeyDown={addMainFocusHandler}
-            value={mainFocusInput}
+            onKeyDown={(e) => continueHandler(e)}
             type="text"
           />
-        )
-      )}
+          <button
+            onClick={(e) => continueHandler(e)}
+            className="btn text-icon continue-btn"
+          >
+            Continue
+            <img className="icon" src={nextIcon} alt="icon" />
+          </button>
+        </div>
+      ) : (
+        <div className="center-align">
+          <h1 className="title">{`${new Date().getHours()}:${getMinuteBelowTen()}`}</h1>
+          <p className="h1 greeting-text">
+            {greetings()}, {userName}.
+          </p>
+          <p className="h2 main-focus-title mt-1">{printMainFocus}</p>
+          {hideMainFocusInput && (
+            <div>
+              <p className="h3 main-focus-title center-text mt-1">
+                What is your main focus for today?
+              </p>
 
-      {!show && (
-        <button
-          onClick={() => continueHandler()}
-          className="btn text-icon continue-btn"
-        >
-          Continue
-          <img className="icon" src={nextIcon} alt="icon" />
-        </button>
+              <input
+                onChange={(e) => setMainFocusInput(e.target.value)}
+                className="name-input"
+                onKeyDown={addMainFocusHandler}
+                value={mainFocusInput}
+                type="text"
+              />
+            </div>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
--- src/Utils/greetings.js
+++ src/Utils/greetings.js
@@ -3,9 +3,10 @@ import { getMinuteBelowTen } from "./getMinuteBelowTen";
 export const greetings = () => {
   const hours = new Date().getHours();
   const minutes = getMinuteBelowTen();
+  console.log(hours,minutes)
   if (hours >= 5 && hours < 11 && minutes < 59) {
     return "Good Morning";
-  } else if (hours > 12 && hours <= 17) {
+  } else if (hours >= 12 && hours <= 17) {
     return "Good Afternoon";
   } else if (hours > 17 && hours <= 19) {
     return "Good Evening";
